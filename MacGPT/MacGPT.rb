b93bfff2010d19420069517a51004af4f5e826f1
Ben de Laat <ben@rootcauseconsulting.nl>
2023-03-14 16:44:32 +0100

    *

---
+++ MacGPT/MacGPT.rb
@@ -0,0 +1,6 @@
+cask 'MacGPT' do
+  version '0.0.1'
+  sha256 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+  url 'https://example.com/download/cask-name-x.x.x.dmg'
+  app 'MacGPT.app'
+end
