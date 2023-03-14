8fafa08c11bca16ac646b9356ccce0386efa18aa
Ben de Laat <ben@rootcauseconsulting.nl>
2023-03-14 12:50:44 +0100

    innitial file creation

---
+++ ImageFocusPlusforMac/ImageFocusPlusforMac.rb
@@ -0,0 +1,7 @@
+cask 'ImageFocusPlusforMac' do
+    version '230106'
+    sha256 'f2adade205d1beebb956e7226558fa77e17953f1'
+    url 'https://www.euromex.com/en/uploads/files/software/ImageFocusPlusForMac_230106.zip'
+    app 'ImageFocusPlusForMac 230106.dmg'
+  end
+  
