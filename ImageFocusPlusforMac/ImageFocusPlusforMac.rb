f9e62f68369224bd337011605c2e37997b50a4b6
Ben de Laat <ben@rootcauseconsulting.nl>
2023-03-14 17:07:39 +0100

    typo correction in desc

--- ImageFocusPlusforMac/ImageFocusPlusforMac.rb
+++ ImageFocusPlusforMac/ImageFocusPlusforMac.rb
@@ -3,7 +3,7 @@ cask "ImageFocusPlusforMac" do
     sha256 "253368b233c9d6f050ea5edc63c1167f6b4e276eca2123a9285dfc681069ecda"
     url "https://www.euromex.com/en/uploads/files/software/ImageFocusPlusForMac_230106.zip"
     name "ImageFocus Plus"
-    desc "Microscopy camera app fro Euromex CMEX USB 2.0 camera's"
+    desc "Microscopy camera app fro Euromex CMEX USB 2.0 cameras"
     app "ImageFocusPlusForMac.app"
   end
   
