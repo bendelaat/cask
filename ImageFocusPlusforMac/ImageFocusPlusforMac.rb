8476ef04d929e1fedefaf5f11a7a76068de87efc
Ben de Laat <ben@rootcauseconsulting.nl>
2023-03-14 17:05:50 +0100

    changed single quote to double

--- ImageFocusPlusforMac/ImageFocusPlusforMac.rb
+++ ImageFocusPlusforMac/ImageFocusPlusforMac.rb
@@ -1,9 +1,9 @@
-cask 'ImageFocusPlusforMac' do
-    version '2.3.1.0'
-    sha256 '253368b233c9d6f050ea5edc63c1167f6b4e276eca2123a9285dfc681069ecda'
-    url 'https://www.euromex.com/en/uploads/files/software/ImageFocusPlusForMac_230106.zip'
+cask "ImageFocusPlusforMac" do
+    version "2.3.1.0"
+    sha256 "253368b233c9d6f050ea5edc63c1167f6b4e276eca2123a9285dfc681069ecda"
+    url "https://www.euromex.com/en/uploads/files/software/ImageFocusPlusForMac_230106.zip"
     name "ImageFocus Plus"
     desc "Microscopy camera app fro Euromex CMEX USB 2.0 camera's"
-    app 'ImageFocusPlusForMac.app'
+    app "ImageFocusPlusForMac.app"
   end
   
