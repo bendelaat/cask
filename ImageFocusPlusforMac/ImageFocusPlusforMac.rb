729d581a00446108a36e953f6908216c8f4bc4c5
Ben de Laat <ben@rootcauseconsulting.nl>
2023-03-14 16:37:01 +0100

    changed sha to correct sha256

--- ImageFocusPlusforMac/ImageFocusPlusforMac.rb
+++ ImageFocusPlusforMac/ImageFocusPlusforMac.rb
@@ -1,6 +1,6 @@
 cask 'ImageFocusPlusforMac' do
     version '230106'
-    sha256 '82017fb8fe228d6a886acb20ac35f5356e10d8b7'
+    sha256 '253368b233c9d6f050ea5edc63c1167f6b4e276eca2123a9285dfc681069ecda'
     url 'https://www.euromex.com/en/uploads/files/software/ImageFocusPlusForMac_230106.zip'
     name "ImageFocus Plus"
     desc "Microscopy camera app fro Euromex CMEX USB 2.0 camera's"
