becbbe9232b3bf720b934d88b4d25505e5988524
Ben de Laat <ben@rootcauseconsulting.nl>
2023-03-14 17:04:04 +0100

    adding version of app

--- ImageFocusPlusforMac/ImageFocusPlusforMac.rb
+++ ImageFocusPlusforMac/ImageFocusPlusforMac.rb
@@ -1,5 +1,5 @@
 cask 'ImageFocusPlusforMac' do
-    version '23.01.06'
+    version '2.3.1.0'
     sha256 '253368b233c9d6f050ea5edc63c1167f6b4e276eca2123a9285dfc681069ecda'
     url 'https://www.euromex.com/en/uploads/files/software/ImageFocusPlusForMac_230106.zip'
     name "ImageFocus Plus"
