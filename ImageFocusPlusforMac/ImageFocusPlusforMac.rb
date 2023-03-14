793630f4592ae50acf42dbffc0d81098f8b21584
Ben de Laat <ben@rootcauseconsulting.nl>
2023-03-14 16:27:19 +0100

    refinement

--- ImageFocusPlusforMac/ImageFocusPlusforMac.rb
+++ ImageFocusPlusforMac/ImageFocusPlusforMac.rb
@@ -2,6 +2,8 @@ cask 'ImageFocusPlusforMac' do
     version '230106'
     sha256 'f2adade205d1beebb956e7226558fa77e17953f1'
     url 'https://www.euromex.com/en/uploads/files/software/ImageFocusPlusForMac_230106.zip'
-    app 'ImageFocusPlusForMac 230106.dmg'
+    name "ImageFocus Plus"
+    desc "Microscopy camera app fro Euromex CMEX USB 2.0 camera's"
+    app 'ImageFocusPlusForMac.app'
   end
   
